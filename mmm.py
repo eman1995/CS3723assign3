5309f9977ec8755088fd70b01b101bf008e7f321
gzm916 <gzm916@my.utsa.edu>
2019-03-02 19:04:48 -0600

    python

--- mmm.py
+++ mmm.py
@@ -2,9 +2,16 @@ def mmm(array):
   min = 100000000000
   max = -min
   sum = 0
-  # TBD
+  for i in array:
+      if array[i] < min:
+          min = array[i]
+      if array[i] > max:
+          max = array[i]
+      sum += array[i]
+  mean = sum / len(array)  
+  return min, max, mean      
 
 data = [((n % 100)+100) for n in range(0,100000)]
 (min, max, mean) = mmm(data)
-print("Min = %d, Max = %d, Sum = %.2f" % (min, max, mean))
+print("Min = %d, Max = %d, Mean = %.2f\tmmm.py" % (min, max, mean))
 
